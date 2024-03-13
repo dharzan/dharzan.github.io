4b46ff9abee703f8c1d291507297ad8fb47d874d
dharzan <dguruparan@beckettcorp.com>
2024-03-13 16:50:29 -0400

    Added Projects

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,13 +41,13 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="/#Linkedin"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Linkedin
             </Link>
             <Link
-              href="/"
+              href="https://dharzan.com/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
