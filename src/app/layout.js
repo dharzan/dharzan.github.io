add85faa513148076a7af9744a38ac84fd4bfad8
dharzan <dguruparan@beckettcorp.com>
2024-03-15 13:53:00 -0400

    added logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Dharsan Guruparan",
   description: "Project Portfolio",
+  icons: {
+    icon: './images/dharzan2.png',
+    shortcut: './images/dharzan2.png',
+    apple: './images/dharzan2.png',
+    other: {
+      rel: './images/dharzan2.png',
+      url: './images/dharzan2.png',
+    },
+  }
 };
 
 export default function RootLayout({ children }) {
